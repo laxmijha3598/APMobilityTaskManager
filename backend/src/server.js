dbe9e7d66e10cc8c0d62a0388c50554b09202fcf
laxmijha3598 <laxmijha3598@gmail.com>
2026-05-12 11:33:26 +0530

    correction in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -14,8 +14,10 @@ import { authRouter } from "./routes/auth.routes.js";
 const app = express();
 
 app.use(cors({
-  origin: "*"
+  origin: "http://localhost:5173",
+  credentials: true
 }));
+
 app.use(express.json({ limit: "1mb" }));
 
 app.get("/health", (_req, res) => res.json({ ok: true }));
